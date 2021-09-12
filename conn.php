6ba85e2596493154408cd285dc89746eb3861438
ryliezachary <losganoryan0808@gmail.com>
2021-09-12 15:17:21 +0800

    update conn.php
    update conn.php

--- conn.php
+++ conn.php
@@ -1,5 +1,5 @@
 <?php
-	$conn = new mysqli('localhost', 'root', '', 'apsystem');
+	$conn = new mysqli('us-cdbr-east-04.cleardb.com', 'bad6f482309708', 'e410cc68', 'heroku_b648a05cf138ff9');
 
 	if ($conn->connect_error) {
 	    die("Connection failed: " . $conn->connect_error);
